d5e4f3ff441bcf1b0419510112d0eb404a8a50a8
Maniish Jangir <manishjangir05012001@gmail.com>
2023-05-12 17:51:25 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/habit_track');
+mongoose.connect('mongodb+srv://manishjangir05012001:12345@cluster0.r5nvte5.mongodb.net/');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'DB Error'));
